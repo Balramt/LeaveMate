2c85941868908afb31357702e004173523369743
Balram <balramt02@gmail.com>
2025-08-04 00:09:15 +0200

    adding code for better ui

---
+++ static/toggle_password.js
@@ -0,0 +1,10 @@
+function togglePassword(id, icon) {
+    const input = document.getElementById(id);
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.textContent = '🙈';
+    } else {
+        input.type = 'password';
+        icon.textContent = '👁';
+    }
+}
